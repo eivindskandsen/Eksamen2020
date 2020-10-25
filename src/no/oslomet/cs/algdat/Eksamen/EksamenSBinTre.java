321701ee743e687f1082541fc4585ddf871d74eb
eivind <eivindskandsen@hotmail.no>
2020-10-25 18:24:32 +0100

    5 completed Begynt på oppgave 6

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -110,18 +110,84 @@ public class EksamenSBinTre<T> {
         return true;                             // vellykket innlegging
 
 
-
-
-
     }
 
 
     public boolean fjern(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
+
+        // kopiert kode fra kompendiumet
+        // ordnet foreldre og tilfeller hvor b er null
+
+            if (verdi == null) return false;  // treet har ingen nullverdier
+
+            Node<T> p = rot, q = null;   // q skal være forelder til p
+
+            while (p != null)            // leter etter verdi
+            {
+                int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+                if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+                else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+                else break;    // den søkte verdien ligger i p
+            }
+            if (p == null) return false;   // finner ikke verdi
+
+            if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
+            {
+                Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
+                if (p == rot){
+                    rot = b;
+                }
+                else if (p == q.venstre) {
+                    q.venstre = b;
+                    if(b!=null){
+                        b.forelder=q;
+                    }
+                }
+                else {
+                    q.høyre = b;
+                    if(b!=null){
+                        b.forelder=q;
+                    }
+
+                }
+            }
+            else  // Tilfelle 3)
+            {
+                Node<T> s = p, r = p.høyre, tNode; // finner neste i inorden
+                while (r.venstre != null)
+                {
+                    s = r;    // s er forelder til r
+                    tNode=r.venstre;
+                    r = r.venstre;
+
+                    tNode.forelder=s;
+                }
+
+                p.verdi = r.verdi;   // kopierer verdien i r til p
+
+                if (s != p){
+                    s.venstre = r.høyre;
+                    tNode=r.høyre;
+                    if(tNode!=null) {
+                        tNode.forelder = s;
+                    }
+                }
+                else {
+                    s.høyre = r.høyre;
+                    tNode=r.høyre;
+                    if(tNode!=null) {
+                        tNode.forelder = s;
+                    }
+                }
+            }
+
+            antall--;   // det er nå én node mindre i treet
+            return true;
+        }
+
 
     public int fjernAlle(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
     }
 
     public int antall(T verdi) {
@@ -137,6 +203,7 @@ public class EksamenSBinTre<T> {
 
 
             Node<T> tempNode = queue.removeFirst();
+
             //legger til tempNode sine barn i køen
 
             if (tempNode.venstre != null) {
@@ -173,46 +240,30 @@ public class EksamenSBinTre<T> {
     private static <T> Node<T> nestePostorden(Node<T> p) {
 
 
+        Node<T> tempForeldre = p.forelder;
 
-        Node<T> tempForeldre;
 
-
-        //finner første node
-        while (true) {
-            if (p.venstre != null) {
-                p = p.venstre;
-            } else if (p.høyre != null) {
-                p = p.høyre;
-            } else {
-                tempForeldre = p.forelder;
-                break;
-            }
-
-        }
 
         // om vi er på notroden
-        if(tempForeldre==null){
+        if (tempForeldre == null) {
             return null;
         }
         //om vi er på en høyre node
-        //fjerner noden etterpå
+
         if (p == tempForeldre.høyre) {
-            p.forelder=null;
-            tempForeldre.høyre=null;
+
             return tempForeldre;
         }
         // om vi er venstre node
-        // fjerner noden etterpå
+
         if (p == tempForeldre.venstre) {
             if (tempForeldre.høyre == null) {
-                p.forelder=null;
-                tempForeldre.venstre=null;
-                return tempForeldre;
-            } else if(tempForeldre.høyre!= null) {
 
-                    p.forelder=null;
-                    tempForeldre.venstre=null;
-                    return førstePostorden(tempForeldre.høyre);
+                return tempForeldre;
+            } else if (tempForeldre.høyre != null) {
+
+
+                return førstePostorden(tempForeldre.høyre);
 
             }
         }
@@ -223,17 +274,18 @@ public class EksamenSBinTre<T> {
     public void postorden(Oppgave<? super T> oppgave) {
 
         // finner første postorden
-        Node<T> p=førstePostorden(rot);
+        Node<T> p = førstePostorden(rot);
 
         //Looper, printer ut og setter p lik nestePostorden av p
-        while(p!= null){
+        while (p != null) {
 
             oppgave.utførOppgave(p.verdi);
-            p=nestePostorden(p);
+            p = nestePostorden(p);
         }
     }
 
     public void postordenRecursive(Oppgave<? super T> oppgave) {
+
 
         postordenRecursive(rot, oppgave);
 
@@ -241,30 +293,84 @@ public class EksamenSBinTre<T> {
     }
 
     private void postordenRecursive(Node<T> p, Oppgave<? super T> oppgave) {
-        /*
-        System.out.println(p);
-        System.out.println(p.venstre);
-        System.out.println(p.høyre);
-        System.out.println(p.forelder);
 
-         */
+        if (p == null) {
+            return;
+        }
 
-        if(p.venstre!= null){
-            postordenRecursive(p.venstre, oppgave);
-        }
-        if(p.høyre!=null) {
-            postordenRecursive(p.høyre, oppgave);
-        }
+
+        postordenRecursive(p.venstre, oppgave);
+
+
+        postordenRecursive(p.høyre, oppgave);
+
 
         oppgave.utførOppgave(p.verdi);
     }
 
     public ArrayList<T> serialize() {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        ArrayList<T> a = new ArrayList<>();
+
+
+
+        ArrayDeque<Node<T>> queue=new ArrayDeque<>();
+        queue.addLast(rot);
+
+        Node<T> nodeTemp;
+
+        while(!queue.isEmpty()) {
+
+            nodeTemp=queue.removeFirst();
+            a.add(nodeTemp.verdi);
+
+            if(nodeTemp.venstre!=null){
+                queue.addLast(nodeTemp.venstre);
+            }
+
+            if(nodeTemp.høyre!= null){
+                queue.addLast(nodeTemp.høyre);
+            }
+
+
+        }
+
+        return a;
+
     }
 
     static <K> EksamenSBinTre<K> deserialize(ArrayList<K> data, Comparator<? super K> c) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+
+
+        EksamenSBinTre<K> binTre= new EksamenSBinTre<>(c);
+
+        /*
+        if(data.get(0)!= null) {
+            binTre.rot = data.get(0);
+        }
+
+        Node<K> p=binTre.rot;
+        Node<K> q=binTre.rot.forelder;
+
+
+         */
+        for(K value : data){
+            binTre.leggInn(value);
+        }
+
+        /*
+        // kopierte litt kode fra leggInn
+
+        while (p != null)       // fortsetter til p er ute av treet
+        {
+            q = p;                                 // q er forelder til p
+            cmp = c.compare(verdi, p.verdi);     // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+        }
+
+
+         */
+
+        return binTre;
     }
 
 
